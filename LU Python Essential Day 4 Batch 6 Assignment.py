e65ecc8176d86987846ed25703da97512f63bdd9
Eva-design4 <68229843+Eva-design4@users.noreply.github.com>
2020-07-23 13:00:37 +0100

    Add files via upload
    kindly let me know once received

---
+++ LU Python Essential Day 4 Batch 6 Assignment.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment 1:
+# Find all occurrence of substring in the given string "What we think we become; we are Python programmers" (we)
+# Print index value.
+# 
+
+# In[1]:
+
+
+str1 = "What we think we become; we are Python programmers"
+str1.count("we")
+
+
+# In[2]:
+
+
+str1.find("we")
+
+
+# In[3]:
+
+
+str1.rfind("we")
+
+
+# # Assignment 2:
+# 
+#  --- islower(), with different kind of strings
+
+# In[1]:
+
+
+"Uganda".islower()
+
+
+# In[2]:
+
+
+"uganda".islower()
+
+
+# In[4]:
+
+
+"PAKISTAN".islower()
+
+
+# ---isupper() with different kind of strings
+
+# In[5]:
+
+
+"POLAND".isupper()
+
+
+# In[6]:
+
+
+"MaLTA".isupper()
+
+
+# In[7]:
+
+
+"Running".isupper()
+
+
+# In[ ]:
+
+
+
+
